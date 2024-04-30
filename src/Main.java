9622766ccd997a2d0c049a39c4136e42997c90f2
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-04-30 12:13:59 +0300

    new main class

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
        PatternMatching pm = new PatternMatching();
-       int[] input = new int[] { 4, 1 ,3, 4, 4, 5, 5, 1};
+       int[] input = new int[] { 5, 5, 3, 7, 7, 8, 8, 3, 5, 5, 1};
        int[] pattern = new int[] {1, 0, -1};
        System.out.println(pm.countMatching(input, pattern));
     }
