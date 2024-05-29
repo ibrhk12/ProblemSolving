09ba756c9bc7200ae6ea3329ac3ad8cc955cbb26
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-05-29 21:46:29 +0300

    Arrays Solution using nested for loop

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        DailyTempraturesArray dta = new DailyTempraturesArray();
-        System.out.println(Arrays.toString(dta.dailyTemperatures(new int[] {45, 43, 45, 43, 45, 31, 32, 33, 50})));
+        TrappingRainWaterArray1 trw = new TrappingRainWaterArray1();
+        System.out.println(trw.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
 }
