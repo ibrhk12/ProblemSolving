01b77010b9b8b43d87dc93afb47926d9bb00fe1b
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-05-29 22:28:54 +0300

    Dynamic Programming an optimized Arrays Solution

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        TrappingRainWaterArray1 trw = new TrappingRainWaterArray1();
-        System.out.println(trw.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
-
+        TrappingRainWaterDynamic trwd = new TrappingRainWaterDynamic();
+        trwd.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
     }
 }
