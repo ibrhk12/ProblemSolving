df3714a42ec7a4e955e6fd8fadee786cb0a84871
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-06-03 18:18:27 +0300

    putting a test case

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws ParseException {
         TrappingRainWaterMonotonicStack trw = new TrappingRainWaterMonotonicStack();
-        System.out.println(trw.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        System.out.println(trw.trap(new int[]{4, 2, 0, 3, 2, 5}));
     }
 }
