58601ef55ac685f4b0ce242d3decaec211371fcb
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-06-10 16:04:52 +0300

    TLE Quick Select Solution

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        MeetingRoomsIITwoPointers mriisl = new MeetingRoomsIITwoPointers();
-        int[][] intervals = new int[][]{{1, 10}, {3, 19}, {2, 7}, {8, 12}, {10, 20}, {11, 30}};
-        int[][] intervals1 = new int[][]{{0,30}, {5, 10}, {15, 20}};
-        System.out.println(mriisl.minMeetingRooms(intervals1));
+        KthLargestElementQuickSelect kleqs = new KthLargestElementQuickSelect();
+        System.out.println(kleqs.findKthLargest(new int[]{1}, 1));
     }
 }
