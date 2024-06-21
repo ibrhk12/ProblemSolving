1495fe1a6f2f0256f4ab9acc49847e9a8c797c52
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-06-21 21:06:35 +0300

    solving the problem of the frequency stack in a fast time complexity

---
+++ src/FreqStackFast.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class FreqStackFast {
+    Map<Integer, Integer> freq;
+    Map<Integer, Stack<Integer>> group;
+    int maxFreq;
+    public FreqStackFast() {
+        maxFreq = 0;
+        freq = new HashMap<>();
+        group = new HashMap<>();
+    }
+
+    public void push(int val) {
+        freq.merge(val, 1, Integer::sum);
+        maxFreq = Math.max(maxFreq, freq.get(val));
+        group.computeIfAbsent(freq.get(val), s -> new Stack<>()).push(val);
+    }
+
+    public int pop() {
+        if(group.get(maxFreq).isEmpty())
+            maxFreq --;
+        int val = group.get(maxFreq).pop();
+        freq.merge(val, -1, Integer::sum);
+        return val;
+    }
+}
