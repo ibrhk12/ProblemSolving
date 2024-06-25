82d6a5ad656922fae7ac88e7a428d2cef1282bc9
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-06-25 15:55:14 +0300

    A solution with hashtable and priotiy queue

--- src/TopKFrequentElements.java
+++ src/TopKFrequentElements.java
@@ -1,2 +1,23 @@
+
+import java.util.*;
+
 public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n : nums)
+            map.merge(n, 1, Integer::sum);
+        List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(map.entrySet());
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(map.size(), new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return Integer.compare(o2.getValue(), o1.getValue());
+            }
+        });
+        pq.addAll(entries);
+        int[] answer = new int[k];
+        for(int i = 0; i < k; i++){
+            answer[i] = pq.poll().getKey();
+        }
+        return answer;
+    }
 }
