f152f376db966128fdcf90cc12a5e72e206eaafe
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-06-26 21:26:50 +0300

    solving quick select and treeMap

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         //FindKthLargesHoareQuickSelect fkle = new FindKthLargesHoareQuickSelect();
         //System.out.println(fkle.findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
-        TopKFrequentElements tkfe = new TopKFrequentElements();
-        System.out.println(Arrays.toString(tkfe.topKFrequent(new int[]{1,2,2,2,3,3,3,3,4,4}, 2)));
+        TopKFrequentElementsTreeMap tkfe = new TopKFrequentElementsTreeMap();
+        System.out.println(Arrays.toString(tkfe.topKFrequent(new int[]{1,2}, 2)));
     }
 }
